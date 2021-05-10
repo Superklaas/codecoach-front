e5629fe9eb9e488db36533886027a9b624065640
Astrid <astrid.pinckers@minfin.fed.be>
2021-05-10 14:31:35 +0200

    migration to 10 done

--- src/app/init-materialize.component.ts
+++ src/app/init-materialize.component.ts
@@ -1,6 +1,7 @@
-import {AfterViewInit} from '@angular/core';
+import { AfterViewInit, Directive } from '@angular/core';
 import * as M from 'materialize-css';
 
+@Directive()
 export abstract class InitMaterializeComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
