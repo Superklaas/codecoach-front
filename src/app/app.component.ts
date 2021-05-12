d6b683db703d75894ed9f9003e8dbdb4a6b68a8e
Ann Cornelis <cornelis.ann@hotmail.com>
2021-05-12 14:15:35 +0200

    added browser title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private _title = 'CodeCoach';
 
+  get title(): string {
+    return this._title;
+  }
 }
