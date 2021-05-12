8816de810e402e5c90d855ec59606a1f2ea420be
Jeroen Debra <jeroen.debra@minfin.fed.be>
2021-05-12 16:30:56 +0200

    STORY 1: added module to navigation-bar.component.spec.ts

--- src/app/layout/navigation-bar/navigation-bar.component.spec.ts
+++ src/app/layout/navigation-bar/navigation-bar.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NavigationBarComponent } from './navigation-bar.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('NavigationBarComponent', () => {
   let component: NavigationBarComponent;
@@ -10,10 +11,11 @@ describe('NavigationBarComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NavigationBarComponent ],
-      imports: [ HttpClientTestingModule ],
+      imports: [ HttpClientTestingModule, RouterTestingModule ],
     })
     .compileComponents();
   }));
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationBarComponent);
