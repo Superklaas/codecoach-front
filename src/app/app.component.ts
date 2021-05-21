f617ef2739d577ec6297afb7fb9ecf12acd3ec17
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-05-21 13:36:31 +0200

    fix materialize init bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {InitMaterializeComponent} from './init-materialize.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends InitMaterializeComponent {
   constructor(private titleService: Title) {
+    super();
     this.titleService.setTitle('CodeCoach');
   }
 }
