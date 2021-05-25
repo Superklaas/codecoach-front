41ca246fcac4cc487e34e6648ecb6d647b72b94f
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-05-25 10:20:02 +0200

    Initialize the parallax in the home component more gracefully

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import { InitMaterializeComponent } from '../init-materialize.component';
-
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent extends InitMaterializeComponent  {
+export class HomeComponent implements AfterViewInit  {
+
+  ngAfterViewInit(): void {
+    const elements = document.getElementsByClassName('parallax');
+    for(let i = 0; i < elements.length; i++) {
+      M.Parallax.init(elements[i]);
+    }
+  }
 
 }
