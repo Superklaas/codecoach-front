ee492c04862e1c333a8236cd1736f8e94c057019
Astrid <astrid.pinckers@minfin.fed.be>
2021-05-25 14:51:34 +0200

    fixed tests

--- src/app/easter-egg/easter-egg.component.spec.ts
+++ src/app/easter-egg/easter-egg.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EasterEggComponent } from './easter-egg.component';
@@ -8,7 +9,8 @@ describe('EasterEggComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EasterEggComponent ]
+      declarations: [ EasterEggComponent ],
+       imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
--- src/app/forgot-password/forgot-password.component.spec.ts
+++ src/app/forgot-password/forgot-password.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
 
@@ -8,7 +11,8 @@ describe('ForgotPasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordComponent ]
+      declarations: [ ForgotPasswordComponent ],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule, RouterTestingModule]
     })
     .compileComponents();
   });
