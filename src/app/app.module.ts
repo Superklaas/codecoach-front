8c3f95e25e5564545ad59518042e14b843100356
Klaas Decock <klaasdc@icloud.com>
2021-05-26 16:32:13 +0200

    display edit coaching topics page when clicking big yellow button or link in menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { EditProfileComponent } from './dashboard/edit-profile/edit-profile.comp
 import { SessionListItemComponent } from './session-list/session-list-item/session-list-item.component';
 import { CommonSessionDetailsComponent } from './session-list/common-session-details/common-session-details.component';
 import { RequestedCoachSessionDetailsComponent } from './session-list/requested-coach-session-details/requested-coach-session-details.component';
+import { EditCoachingTopicsComponent } from './dashboard-coach/edit-coaching-topics/edit-coaching-topics.component';
 import { WaitingFeedbackCoachSessionDetailsComponent } from './session-list/waiting-feedback-coach-session-details/waiting-feedback-coach-session-details.component';
 import { CoachFeedbackFormComponent } from './session-list/coach-feedback-form/coach-feedback-form.component';
 import { SmileySelectorComponent } from './session-list/smiley-selector/smiley-selector.component';
@@ -74,8 +75,10 @@ export function HttpLoaderFactory(http: HttpClient) {
     RequestedCoachSessionDetailsComponent,
     WaitingFeedbackCoachSessionDetailsComponent,
     CoachFeedbackFormComponent,
-    SmileySelectorComponent
+    SmileySelectorComponent,
 
+    RequestedCoachSessionDetailsComponent,
+    EditCoachingTopicsComponent
   ],
   imports: [
     BrowserModule,
