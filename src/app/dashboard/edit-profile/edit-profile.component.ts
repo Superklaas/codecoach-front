26f563c4d6c16bf3893c4cebec23cb62483b1528
Astrid <astrid.pinckers@minfin.fed.be>
2021-05-27 15:26:04 +0200

    story 7

--- src/app/dashboard/edit-profile/edit-profile.component.ts
+++ src/app/dashboard/edit-profile/edit-profile.component.ts
@@ -63,7 +63,7 @@ export class EditProfileComponent implements OnInit {
   update() {
     this.userService.update(this._editForm.value, +this.authService.getId()).subscribe(
       (_ => alert("Your changes have been saved")),
-      (error =>  this._editForm.setErrors({serverError: error.error.message}))
+      (error =>  this._editForm.setErrors({serverError: 'Oops, something went wrong'}))
     );
   }
   
