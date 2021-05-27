2903a5306e3cc170ef8e938cec24e08705abe76b
Klaas Decock <klaasdc@icloud.com>
2021-05-27 10:51:48 +0200

    STORY 37: display all existing topics in datalist of input when editing coaching topics

--- src/app/dashboard-coach/edit-coaching-topics/edit-coaching-topics.component.ts
+++ src/app/dashboard-coach/edit-coaching-topics/edit-coaching-topics.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {Topic} from "../../model/Topic";
+import {TopicService} from "../../service/topic.service";
 
 @Component({
   selector: 'app-edit-coaching-topics',
@@ -8,14 +11,17 @@ import {FormBuilder, FormControl} from "@angular/forms";
 })
 export class EditCoachingTopicsComponent implements OnInit {
 
+  topics$: Observable<Topic[]>;
+
   private _editForm = this.formBuilder.group({
     topic1: new FormControl("",),
     topic2: new FormControl("",)
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private topicService: TopicService) { }
 
   ngOnInit(): void {
+    this.topics$ = this.topicService.getAllTopics();
   }
 
   get editForm() {
