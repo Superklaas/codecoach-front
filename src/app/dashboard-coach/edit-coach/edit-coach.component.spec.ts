e04c5a5b3dad83d036a3ef53dbcfad53ece3dae7
Astrid <astrid.pinckers@minfin.fed.be>
2021-05-27 15:47:23 +0200

    attempt to fix test failures

--- src/app/dashboard-coach/edit-coach/edit-coach.component.spec.ts
+++ src/app/dashboard-coach/edit-coach/edit-coach.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { EditCoachComponent } from './edit-coach.component';
 
@@ -8,7 +12,8 @@ describe('EditCoachComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditCoachComponent ]
+      declarations: [ EditCoachComponent ],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule,]
     })
     .compileComponents();
   });
