eed5cd90b892ea9b068a137630d0037a3234da2c
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-05-27 11:27:21 +0200

    reworked loginform a bit to have better validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
 import {AuthenticationService} from '../authentication/authentication.service';
 import {Router} from '@angular/router';
 import {InitService} from "../materialize/init.service";
@@ -11,15 +11,14 @@ import {InitService} from "../materialize/init.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  error;
-  success;
-  loginForm;
+  loginForm = this.formBuilder.group({
+    username: new FormControl("", [Validators.required, Validators.pattern(/.*@.*/)]),
+    password: new FormControl("",
+      [Validators.required, Validators.minLength(8),
+        Validators.pattern(/.*[0-9]+.*/), Validators.pattern(/.*[A-Z]+.*/), Validators.pattern(/.*[a-z]+.*/)]),
+  });;
 
   constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService, private router: Router, private initService: InitService) {
-    this.loginForm = this.formBuilder.group({
-      username: '',
-      password: ''
-    });
   }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
@@ -27,23 +26,28 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(loginData) {
-    this.success = false;
-    this.error = false;
+    this.loginForm.markAllAsTouched();
+
+    if (!this.loginForm.valid) {
+      this.loginForm.setErrors({credentials: "wrong"});
+      return
+    }
+
     this.authenticationService.login(loginData)
       .subscribe(
         (_ => {
-          this.success = true;
-          this.initService.initDropdowns();
           this.router.navigateByUrl(`/dashboard`);
+          this.initService.initDropdowns();
+          this.loginForm.reset();
         }),
         (error => this.addErrorToForm(error))
       );
-    this.loginForm.reset();
   }
 
   addErrorToForm(errorResponse) {
     if (errorResponse.error.status === 400) {
       this.loginForm.setErrors({serverError: errorResponse.error.message});
+      this.loginForm.setErrors({credentials: "wrong"});
     }else{
       this.loginForm.setErrors({ serverError : 'Error: oops something went wrong...'});
     }
