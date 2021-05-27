e92f0e2eee59372508da87768472c0737106bd33
Astrid <astrid.pinckers@minfin.fed.be>
2021-05-27 15:47:48 +0200

    small changes

--- src/app/dashboard-coach/edit-coach/edit-coach.component.spec.ts
+++ src/app/dashboard-coach/edit-coach/edit-coach.component.spec.ts
@@ -1,8 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 
 import { EditCoachComponent } from './edit-coach.component';
 
