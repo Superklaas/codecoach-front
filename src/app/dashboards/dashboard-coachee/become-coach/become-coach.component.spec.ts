9ce4afdafe38f53ab5df6206d51f1de40c9e0978
Astrid <astrid.pinckers@minfin.fed.be>
2021-06-01 17:04:55 +0200

    test fix

--- src/app/dashboards/dashboard-coachee/become-coach/become-coach.component.spec.ts
+++ src/app/dashboards/dashboard-coachee/become-coach/become-coach.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BecomeCoachComponent } from './become-coach.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('BecomeCoachComponent', () => {
   let component: BecomeCoachComponent;
@@ -11,7 +12,7 @@ describe('BecomeCoachComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule,
-        RouterTestingModule, ],
+        RouterTestingModule, FormsModule, ReactiveFormsModule],
       declarations: [ BecomeCoachComponent ]
     })
     .compileComponents();
