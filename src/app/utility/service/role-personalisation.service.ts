4d0bc5641b4871cf7f16f74558a3e12ab6fc7106
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-06-02 15:14:59 +0200

    Fix inconsistent personalisation with route guards
    WHen a user tries to access a route-guard protected resource and that
    user does not have access to that resource, the personalisation service
    might still set the color for that resource.
    
    The personalisation service listened to the NavigationStart event which
    is launched even if the router cannot go to the selected path.
    
    Changing it to the NavigationEnd event should fix it.

--- src/app/utility/service/role-personalisation.service.ts
+++ src/app/utility/service/role-personalisation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { NavigationStart, Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Injectable({
@@ -10,8 +10,8 @@ export class RolePersonalisationService {
 
   constructor(private router: Router) {
     this.router.events
-    .pipe(filter(event => event instanceof NavigationStart))
-    .subscribe((ev:NavigationStart) => {
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((ev:NavigationEnd) => {
       this.color='yellow darken-2';
       if(ev.url.startsWith('/dashboard-coach')){
         this.color='light-blue';
