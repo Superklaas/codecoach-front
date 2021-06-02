9957f06b216d3bc6eb06db57f7a771d9f3744d84
Astrid <astrid.pinckers@minfin.fed.be>
2021-06-02 11:18:20 +0200

    search implemented

---
+++ src/app/utility/pipe/search-coach.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchCoachPipe } from './search-coach.pipe';
+
+describe('SearchCoachPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchCoachPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
