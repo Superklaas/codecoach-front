1ddd54a4abaec094a11cb80133eb84c87501dd63
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-06-02 11:16:33 +0200

    rewrote admin & authorization guards

--- src/app/authorization/admin.guard.ts
+++ src/app/authorization/admin.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
@@ -12,11 +13,10 @@ export class AdminGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.getRole() !== 'ADMIN') {
-      this.router.navigate(['/dashboard']);
-      return false;
-    }
-    return true;
+      return this.authService.session$
+      .pipe(map(session => session.isAdmin()))
+      .pipe(tap(authorized => {if (!authorized) this.router.navigate(['/dashboard'])}));
+
   }
 
 }
