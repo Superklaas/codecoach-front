f4eacfc3f68eb58aa47b680ec2dc57824838cb80
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-06-04 11:00:35 +0200

    fix failing unit tests

--- src/app/dashboards/dashboard-coach/request-change-coaching-topic/request-change-coaching-topic.component.spec.ts
+++ src/app/dashboards/dashboard-coach/request-change-coaching-topic/request-change-coaching-topic.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RequestChangeCoachingTopicComponent } from './request-change-coaching-topic.component';
 
@@ -8,7 +10,8 @@ describe('RequestChangeCoachingTopicComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RequestChangeCoachingTopicComponent ]
+      declarations: [ RequestChangeCoachingTopicComponent ],
+      imports: [ HttpClientTestingModule, RouterTestingModule, ],
     })
     .compileComponents();
   });
